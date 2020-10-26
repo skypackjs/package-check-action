0733cf1b5f478eb66e0d76ed8f6fdfe9987a4d07
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-10-26 11:41:22 -0700

    fix(deps): bump `@skypack/package-check` to v0.2.2 and pin version (#2)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const core = require("@actions/core");
 
 main();
@@ -6,15 +7,15 @@ async function main() {
   const cwd = core.getInput("cwd");
 
   try {
+    const args = [];
     if (cwd) {
-      core.debug(`cwd input is set to "${cwd}". Changing working directory`);
-      process.chdir(cwd);
-      core.info(`working directory is ${process.cwd()}`);
+      core.info(`Setting working directory to ${path.resolve(cwd)}`);
+      args.push("--cwd", path.resolve(cwd));
     }
 
     const cli = require("@skypack/package-check");
     const run = cli.run || cli.cli || cli.default;
-    await run();
+    await run(args);
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
